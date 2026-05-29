8477bc0ad6035e299f811555923bd0ff05c2527f
OscarFragosoRojas <oscarfragosorojas@gmail.com>
2026-05-29 11:14:46 -0600

    feat: add FloatingBadge component and associated configuration data structures

---
+++ src/data/badge.ts
@@ -0,0 +1,8 @@
+import type { FloatingBadgeProps } from "../types/types";
+
+export const badge: FloatingBadgeProps = {
+  text: "Nutrición",
+  subText: "Clínica & Deportiva",
+  icon: "",
+  position: "top-left",
+};
--- src/types/types.ts
+++ src/types/types.ts
@@ -2,3 +2,10 @@ export interface Props {
   title: string;
   description?: string;
 }
+
+export interface FloatingBadgeProps {
+  text: string;
+  subText: string;
+  icon?: string;
+  position?: "top-left" | "top-right" | "bottom-left" | "bottom-right";
+}
